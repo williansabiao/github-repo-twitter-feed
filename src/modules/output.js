fe634bdff0117363175ea5ebe1fc07d2c4e7fb2c
Willian R. Sabião <willianjedai@gmail.com>
2017-08-28 23:37:11 +0100

    adding callback to write function to remove warn

--- src/modules/output.js
+++ src/modules/output.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 const writeFile = (filePath, data) => {
-  fs.writeFile(filePath, (typeof data === 'object') ? JSON.stringify(data) : data, 'utf8');
+  fs.writeFile(filePath, (typeof data === 'object') ? JSON.stringify(data) : data, 'utf8', (err) => (err) ? console.log('Error:' + err) : true);
 }
 const deleteFile = (filePath, cb = () => {}) => {
   fs.unlink(filePath, (err) => {
